456ee1e3a936591f1fd416f65370bf6b73bd2eb8
pengchujin <2093959501@qq.com>
2026-09-08 22:18:37 +0800

    Revert "Use measured row positions for Mac rule and candidate reordering"
    This reverts commit 9886dad068ab63fad0a08a5723631f69fdecebcd.

--- TowerTests/RepositoryConsistencyTests.swift
+++ TowerTests/RepositoryConsistencyTests.swift
@@ -257,8 +257,7 @@ final class RepositoryConsistencyTests: XCTestCase {
 
         XCTAssertFalse(source.contains("private struct ImportedRuleSchemeRow"))
         XCTAssertFalse(source.contains(".swipeActions"))
-        XCTAssertFalse(cardSource.contains("DragGesture(minimumDistance:"))
-        XCTAssertFalse(importedSource.contains("DragGesture(minimumDistance:"))
+        XCTAssertFalse(source.contains("DragGesture(minimumDistance:"))
         XCTAssertTrue(importedSource.contains("onEdit: { editingImportedScheme = scheme }"))
         XCTAssertTrue(importedSource.contains("onDelete: { pendingDeletion = scheme }"))
         XCTAssertTrue(cardSource.contains(".contextMenu"))
